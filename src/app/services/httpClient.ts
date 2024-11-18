d2c07ab5e0574a2b63bab23910cf81f3819b667a
Wellington Eduardo <wellingtoneduardo10@outlook.com>
2024-11-18 10:05:20 -0300

    I configured the sanctum and created the login route

--- src/app/services/httpClient.ts
+++ src/app/services/httpClient.ts
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
 export const httpClient = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: 'http://localhost:8000',
+  withXSRFToken: true,
   withCredentials: true,
+
+  headers: {
+    'Accept': 'application/json',
+
+  }
 });
--- src/view/page/Login/index.tsx
+++ src/view/page/Login/index.tsx
@@ -1,4 +1,45 @@
+import { FormEvent, useState } from "react";
+import { Form } from "../../components/Form";
+import { Input } from "../../components/Input";
+import { httpClient } from "../../../app/services/httpClient";
+
+
+
 
 export function Login() {
-  return <h1>Login</h1>
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const getCsrfToken = async () => {
+    return await httpClient.get('/sanctum/csrf-cookie');
+  };
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    await getCsrfToken();
+
+
+
+
+    const loginResponse = await httpClient.post('/login', {
+      email,
+      password,
+    });
+    // console.log('Login Response:', loginResponse);
+
+
+  }
+  return (
+    <div>
+      <h1>Login</h1>
+
+      <div className="flex justify-center">
+        <Form onHandleSubmit={handleSubmit}>
+          <Input value={email} type="email" name="email" placeholder="Email" onHandleClick={setEmail} />
+          <Input value={password} type="password" name="password" placeholder="senha" onHandleClick={setPassword} />
+
+        </Form>
+      </div>
+    </div>
+  )
 }
